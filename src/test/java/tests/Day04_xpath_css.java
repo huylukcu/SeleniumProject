93b2348770c3417359c11c7eb62355bf8e325204
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-15 22:39:23 -0600

    commit

--- src/test/java/tests/Day04_xpath_css.java
+++ src/test/java/tests/Day04_xpath_css.java
@@ -2,8 +2,12 @@ package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Day04_xpath_css {
     WebDriver driver;
@@ -11,7 +15,18 @@ public class Day04_xpath_css {
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("gihttps://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
-
+    @Test
+    public void loginTest(){
+    //locating username and typing username
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+    //locating password and typing password
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+    //locating login button and click
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
 }
+
