ae88337b5b1919ff4b401625ee8b030d2fc47117
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-18 10:06:56 -0600

    commit

---
+++ src/test/java/tests/Day07_Alerts.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Day07_Alerts extends TestBase {
+    @Test
+    public void acceptAlert() throws InterruptedException {
+        //acceptAlert() => click on the first alert,
+        driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+
+        //verify the text “I am a JS Alert” ,
+        String actualAlertText = driver.switchTo().alert().getText();
+        Assert.assertEquals("I am a JS Alert",actualAlertText);
+
+        //click OK ,
+        driver.switchTo().alert().accept();
+
+        //and Verify “You successfully clicked an alert”
+        String actualResult = driver.findElement(By.id("result")).getText();
+        Assert.assertEquals("You successfully clicked an alert",actualResult);
+
+        Thread.sleep(2000);
+
+
+    }
+}
+
