ca87cc0d972e9e5f19dfc82f4e24d39630f5af4c
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-18 21:05:25 -0600

    commit

--- src/test/java/tests/Day07_TestBaseDemo.java
+++ src/test/java/tests/Day07_TestBaseDemo.java
@@ -6,14 +6,17 @@ import utilities.TestBase;
 public class Day07_TestBaseDemo extends TestBase {
 
     @Test
-    public  void test1(){
+    public void test1() {
         driver.get("https://testcenter.techproeducation.com/");
         System.out.println(driver.getTitle());
-
     }
     @Test
     public void test2(){
-        driver.get("https://www.techproeducation.com/");
-        System.out.println(driver.getTitle());
+        driver.get("https://techproeducation.com/");
+
     }
+
+
+
+
 }
