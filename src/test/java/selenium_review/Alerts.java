9829b6a5f2f7d5146172eafe0f61a64990196e5b
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-19 00:05:26 -0600

    commit

--- src/test/java/selenium_review/Alerts.java
+++ src/test/java/selenium_review/Alerts.java
@@ -1,5 +1,6 @@
 package selenium_review;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities.TestBase;
@@ -11,7 +12,10 @@ public class Alerts extends TestBase {
         driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
         driver.findElement(By.xpath("//button[@class='btn btn-primary'][1]")).click();
         //verify the text "I am a Js Alert"
-        String alertText = driver.switchTo().alert().getText();//special
+        String actualAlertText= driver.switchTo().alert().getText();//special
+        Assert.assertEquals("I am a Js Alert",actualAlertText);
+        //click OK,
+        driver.switchTo().alert().accept();
 
     }
 }
