3b0a87c6b290050a7e355a1001c041d1207266c5
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-21 12:38:07 -0600

    commit

---
+++ src/test/java/tests/Day08_Actions02.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.Test;
+
+public class Day08_Actions02 {
+    /*
+        Given
+                Go to https://testpages.herokuapp.com/styled/csspseudo/css-hover.html
+        When
+                Hover over "Hover Div"
+        And
+                Click on “Can you click me?” link.
+        Then
+                Verify the text "Action Complete".
+        */
+    @Test
+    public void hoverOverTest(){
+
+    }
+
+}
