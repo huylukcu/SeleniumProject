c7dd941742efb15a6628b6cded806e378dda88d2
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-24 10:11:08 -0600

    commit

---
+++ src/test/java/tests/Day10_AutoComplete.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Day10_AutoComplete {
+    /*
+    Given user is on https://testcenter.techproeducation.com/index.php?page=autocomplete
+    When user type “uni” in the search box
+    And select the ‘United Kingdom’ from the suggestions
+    And click on submit button
+    Then verify the result contains ‘United Kingdom’
+     */
+
+
+
+
+}
