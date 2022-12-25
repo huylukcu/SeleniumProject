e5c023c9d5295c5d6cc70d12598a123e4e0a7844
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-25 11:56:36 -0600

    commit

---
+++ src/test/java/selenium_review/DragAndDrop.java
@@ -0,0 +1,21 @@
+package selenium_review;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class DragAndDrop extends TestBase {
+    @Test
+    public void dragAndDropTest() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        WebElement sourse = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+
+        Actions actions = new Actions(driver);
+    }
+
+
+}
