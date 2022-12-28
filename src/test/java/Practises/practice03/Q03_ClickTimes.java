96168eae99d918217ea9fdb80d7038c727748f42
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-12-27 19:27:10 -0600

    commit

---
+++ src/test/java/Practises/practice03/Q03_ClickTimes.java
@@ -0,0 +1,18 @@
+package Practises.practice03;
+
+import utilities.TestBase;
+
+public class Q03_ClickTimes extends TestBase {
+   /* Given
+    Go to http://the-internet.herokuapp.com/add_remove_elements/
+    When
+    Click on the "Add Element" button 100 times
+            And
+    Click on the "Delete" button 20 times
+            Then
+             Assert that 20 buttons were deleted.
+//        */
+
+
+
+}
