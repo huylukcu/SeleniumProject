26180ca85a4d7f4083e117c98d9c7d7d69a5ffa5
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-01 11:14:19 -0600

    commit

--- src/test/java/Practises/homework/Q02.java
+++ src/test/java/Practises/homework/Q02.java
@@ -1,12 +1,12 @@
 package Practises.homework;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
-public class Den extends TestBase {
+public class Q02 extends TestBase {
     @Test
     public void set() {
        /* Create a new class : GoogleSearchTest
@@ -16,11 +16,17 @@ public class Den extends TestBase {
         And print how many related results displayed on Google
         */
 
+       // When user goes to https://www.google.com/
         driver.get("https://www.google.com/");
-        // Search for “porcelain teapot”
+        //Search for “porcelain teapot”
         driver.findElement(By.name("q")).sendKeys("porcelain teapot"+ Keys.ENTER);
         String searchItem = driver.findElement(By.id("result-stats")).getText();
         System.out.println(searchItem);
-
+        //And print how many related results displayed on Google
+        WebElement resultText = driver.findElement(By.id("result-stats"));
+        String result = resultText.getText();
+        System.out.println(result);
     }
+
+
 }
