84fe55f5d64fb57dd8cf98888fbc7087a53aac29
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-01 12:33:40 -0600

    commit

--- src/test/java/Practises/homework/Q03.java
+++ src/test/java/Practises/homework/Q03.java
@@ -8,7 +8,8 @@ import utilities.TestBase;
 
 public class Q03 extends TestBase {
     /*
-    Then create 3 different test methods using the following names. And Go to google.
+    Then create 3 different test methods using the following names.
+    And Go to google.
     titleTest =>Verify if google title = “Google”
     imageTest => Verify if google image displays or not
     gmailLinkTest => Verify if the Gmail link is displayed or not
@@ -24,14 +25,13 @@ public class Q03 extends TestBase {
         System.out.println("Title : " + title);
         Assert.assertEquals("Google", title);
     }
-
-    //2.imageTest => Verify if google image displays or not@Test
-    @Test
-    public void imageTest() {
+    //2.imageTest => Verify if google image displays or not
+     @Test
+     public void imageTest() {
         driver.get("https://www.google.com/");
         WebElement image = driver.findElement(By.id("hplogo"));
         Assert.assertTrue(image.isDisplayed());
-    }
+     }
 
     //3.Verify if the Gmail link is displayed
     @Test
