344eea58b200e3557a75b488f72f41d2a9c34e82
huylukcu <84544888+huylukcu@users.noreply.github.com>
2022-12-31 21:46:42 -0600

    Update Q01.java

--- src/test/java/Practises/homework/Q01.java
+++ src/test/java/Practises/homework/Q01.java
@@ -1,11 +1,3 @@
 package Practises.homework;
 
 public class Q01 {
-//    Create A Class: AmazonDropdown
-//    Create A Method dropdownTest
-//    Go to https://www.amazon.com/
-//    Find the element of the dropdown element. HINT: By.id(“searchDropdownBox")
-//    Print the first selected option and assert if it equals “All Departments”.
-//    If it fails, then comment that code out and continue rest of the test case.
-
-}
