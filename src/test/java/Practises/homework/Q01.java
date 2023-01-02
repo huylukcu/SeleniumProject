2c0d063d5287ddbdd2862092990d6f3f4b2936ea
huylukcu <84544888+huylukcu@users.noreply.github.com>
2023-01-02 10:31:59 -0600

    Update Q01.java

--- src/test/java/Practises/homework/Q01.java
+++ src/test/java/Practises/homework/Q01.java
@@ -22,7 +22,7 @@ public class Q01 {
     Select the 4th option by index (index of 3) and assert if the name is “Amazon Devices”.(after you select you need to use get first selected option method). If it fails, then comment that code out and continue rest of the test case.
     Print all of the dropdown options-getOptions(); method returns the List<WebElement>. Using loop, print all options.
     Print the the total number of options in the dropdown
-    Assert if ‘Appliances’ is a drop down option. Print true if “Appliances” is an option. Print false otherwise.
+    Assert if "Computers" is a drop down option. Print true if "Computers" is an option. Print false otherwise.
     BONUS: Assert if the dropdown is in Alphabetical Order
     */
     WebDriver driver;
