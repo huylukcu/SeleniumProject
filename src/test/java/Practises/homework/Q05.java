5ec9f1530931c4434036e2ddc881b15ab247eba8
huylukcu <84544888+huylukcu@users.noreply.github.com>
2023-01-03 19:07:01 -0600

    Update Q05.java

--- src/test/java/Practises/homework/Q05.java
+++ src/test/java/Practises/homework/Q05.java
@@ -10,16 +10,15 @@ public class Q05 extends TestBase {
     @Test
     public void test(){
 
-    //When user goes to https://jqueryui.com/accordion/
+     //When user goes to https://jqueryui.com/accordion/
         driver.get("https://jqueryui.com/accordion/");
         driver.switchTo().frame(0);
 
-    //And user clicks on Section2 accordion
+     //And user clicks on Section2 accordion
         driver.findElement(By.id("ui-id-3")).click();
 
-    // Verify the text contains “Sed non urna.”
+     // Verify the text contains “Sed non urna.”
       WebElement select2 = driver.findElement(By.("//p[contains((),'Sed non urna')]");
-        Assert.assertEquals(co)
-//Assert.assertTrue(driver.findElement(By.xpath("//*[@id='ui-id-4']/p")).getText().contains("Sed non urna."));
+     //Assert.assertTrue(driver.findElement(By.xpath("//*[@id='ui-id-4']/p")).getText().contains("Sed non urna."));
 
 }}
