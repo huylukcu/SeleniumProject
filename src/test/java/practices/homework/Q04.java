b3489dbb1b9230d061cc965b883203de58b069d1
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-09 21:26:59 -0600

    commit

--- src/test/java/practices/homework/Q04.java
+++ src/test/java/practices/homework/Q04.java
@@ -1,4 +1,14 @@
 package practices.homework;
 
-public class Q04 {
+import utilities.TestBase;
+
+public class Q04 extends TestBase {
+/*
+    Create a new class: AmazonSearch
+    //TC01_As user I want to know how many item are there on amazon in the first page after I search “porcelain teapot”?
+    //TC02_Order the tea pot prices, find the min, max, and average price to the nearest cent.
+ */
+
+
+
 }
